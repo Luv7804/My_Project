4c4433364f1b166ede839f72081cf7bd5b44d805
Luv7804 <chintanviradiya360311@gmail.com>
2022-08-15 10:05:52 +0530

    Recycler View

--- app/src/main/java/com/example/my_project/MainActivity.java
+++ app/src/main/java/com/example/my_project/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.my_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,9 +15,24 @@ import com.airbnb.lottie.LottieAnimationView;
 import java.math.BigInteger;
 
 public class MainActivity extends AppCompatActivity {
-    @Override
+    Button register,login;
+    @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        register = findViewById(R.id.registration);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+            }
+        });
+        login =findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,MainActivity3.class));
+            }
+        });
     }
 }
