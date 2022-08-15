51bb89fad904d7f1e55c2b16e55d576206cba5e3
Luv7804 <chintanviradiya360311@gmail.com>
2022-08-15 09:59:36 +0530

    card

--- app/src/main/java/com/example/my_project/MainActivity.java
+++ app/src/main/java/com/example/my_project/MainActivity.java
@@ -14,31 +14,9 @@ import com.airbnb.lottie.LottieAnimationView;
 import java.math.BigInteger;
 
 public class MainActivity extends AppCompatActivity {
-    Button check;
-    LottieAnimationView lottie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        lottie = findViewById(R.id.lottie);
-//        lottie.setAnimation(R.raw.loader);
-//        lottie.playAnimation();
-//        lottie.loop(true);
-
-        check =findViewById(R.id.button_check);
-        Intent iNext;
-        iNext = new Intent(MainActivity.this,MainActivity2.class);
-        iNext.putExtra("name","Chintan");
-        iNext.putExtra("enno",148);
-        check.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                startActivity(iNext);
-                Toast.makeText(MainActivity.this, "Textbox", Toast.LENGTH_SHORT).show();
-
-
-            }
-        });
     }
 }
