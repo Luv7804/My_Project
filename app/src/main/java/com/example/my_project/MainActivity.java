a70f0901f5f66548f2188e34fd52e8429c8c8877
Luv7804 <chintanviradiya360311@gmail.com>
2022-08-25 10:23:22 +0530

    Registration and login

--- app/src/main/java/com/example/my_project/MainActivity.java
+++ app/src/main/java/com/example/my_project/MainActivity.java
@@ -1,21 +1,16 @@
 package com.example.my_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.airbnb.lottie.LottieAnimationView;
-
-import java.math.BigInteger;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Button register,login;
+    Button register, log;
+
     @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,14 +19,14 @@ public class MainActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
             }
         });
-        login =findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
+        log = findViewById(R.id.login);
+        log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,MainActivity3.class));
+                startActivity(new Intent(MainActivity.this, MainActivity3.class));
             }
         });
     }
