98e80756ce7f5e835a646dca18097d59451b1a1c
Luv7804 <chintanviradiya360311@gmail.com>
2022-08-26 10:31:25 +0530

    register bundel

--- app/src/main/java/com/example/my_project/MainActivity4.java
+++ app/src/main/java/com/example/my_project/MainActivity4.java
@@ -1,14 +1,19 @@
 package com.example.my_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity4 extends AppCompatActivity {
-
+    TextView email = findViewById(R.id.email);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        Intent bundle = new Intent();
+        String name = bundle.getStringExtra("name");
+        email.setText(name);
     }
 }
