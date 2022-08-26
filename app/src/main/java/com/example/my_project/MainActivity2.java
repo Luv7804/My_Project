9e91525d161fe923f6dab12c5f4dc4e8da3d53f8
Luv7804 <chintanviradiya360311@gmail.com>
2022-08-26 09:31:24 +0530

    Registration and login

--- app/src/main/java/com/example/my_project/MainActivity2.java
+++ app/src/main/java/com/example/my_project/MainActivity2.java
@@ -1,27 +1,23 @@
 package com.example.my_project;
 
-import static android.view.View.*;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.graphics.drawable.Animatable;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.math.BigInteger;
-
 public class MainActivity2 extends AppCompatActivity {
+    Button sign_up = findViewById(R.id.signup);
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity2.this, MainActivity4.class));
+            }
+        });
 
-        }
+    }
     }
