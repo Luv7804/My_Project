eb9c68fd24d81c2324508ec7625487b0f165b0d9
Luv7804 <chintanviradiya360311@gmail.com>
2022-09-08 21:23:30 +0530

    RecyclerView

--- app/src/main/java/com/example/my_project/MainActivity.java
+++ app/src/main/java/com/example/my_project/MainActivity.java
@@ -1,22 +1,18 @@
 package com.example.my_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.airbnb.lottie.LottieAnimationView;
-
-import java.math.BigInteger;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerContact);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/example/my_project/MainActivity2.java
+++ app/src/main/java/com/example/my_project/MainActivity2.java
@@ -1,70 +1,14 @@
 package com.example.my_project;
 
-import static android.view.View.*;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.graphics.drawable.Animatable;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.math.BigInteger;
-
 public class MainActivity2 extends AppCompatActivity {
-    TextView check2,name,enno;
-    Button move;
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        check2 = findViewById(R.id.textView);
-        name = findViewById(R.id.name);
-        move = findViewById(R.id.move);
-
-        move.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Animation animate = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.move);
-                Animation ani = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.alpha);
-                name.startAnimation(ani);
-//                name.startAnimation(animate);
-
-            }
-        });
-
-        Intent fromact = getIntent();
-        String myName = fromact.getStringExtra("name");
-        int enNumber =fromact.getIntExtra("enno",0);
-//        int en = fromact.getIntExtra("enno",0);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                name.setText(myName+"="+enNumber);
-
-//                enno.setText(en);
-
-
-            }
-        },2000);
-
-
-        check2.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity2.this, "TicTacToe", Toast.LENGTH_SHORT).show();
-
-                startActivity(new Intent(MainActivity2.this,MainActivity3.class));
-            }
-        });
 
         }
     }
