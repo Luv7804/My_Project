6a010d400a321974661909332e5b3f8b04deee17
Luv7804 <chintanviradiya360311@gmail.com>
2022-12-31 14:26:27 +0530

    APMC Home Page

--- app/src/main/java/com/example/my_project/MainActivity.java
+++ app/src/main/java/com/example/my_project/MainActivity.java
@@ -1,32 +1,29 @@
+
 package com.example.my_project;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
-    Button register, log;
-    @SuppressLint("MissingInflatedId")
+    private FirebaseAuth mAuth;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        register = findViewById(R.id.registration);
-        register.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+        new Timer().schedule(new TimerTask() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, MainActivity2.class));
-            }
-        });
-        log = findViewById(R.id.login);
-        log.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            public void run() {
                 startActivity(new Intent(MainActivity.this, MainActivity3.class));
             }
-        });
+        }, 3000);
     }
 }
