efb3a55161f2f457f8153737e9f274f9973e6cfb
Luv7804 <chintanviradiya360311@gmail.com>
2023-01-12 22:41:51 +0530

    APMC TabLayout Incomplete

--- app/src/main/java/com/example/my_project/JanasiFragment.java
+++ app/src/main/java/com/example/my_project/JanasiFragment.java
@@ -7,11 +7,6 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link JanasiFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class JanasiFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -27,14 +22,7 @@ public class JanasiFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment JanasiFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static JanasiFragment newInstance(String param1, String param2) {
         JanasiFragment fragment = new JanasiFragment();
