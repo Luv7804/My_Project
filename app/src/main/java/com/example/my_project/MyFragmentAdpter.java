c3dcf5b8e0dcf0493444c3f71bdb6d1429e84c02
Luv7804 <chintanviradiya360311@gmail.com>
2023-01-12 22:30:28 +0530

    APMC TabLayout Incomplete

--- app/src/main/java/com/example/my_project/MyFragmentAdpter.java
+++ app/src/main/java/com/example/my_project/MyFragmentAdpter.java
@@ -14,9 +14,9 @@ public class MyFragmentAdpter extends FragmentStateAdapter {
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        if (position == 1) {
+        if (position == 0) {
             return new JanasiFragment();
-        } else if (position == 2) {
+        } else if (position == 1) {
             return new Sakbhaji_Fragment();
         } else {
             return new FalFragment();
